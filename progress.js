3f53d02dabed7c559b57849ce9220fb4cddea276
SHIIV07 <mlynariktomo@gmail.com>
2025-01-19 16:49:57 +0100

    Update progress.js
    Arithmetic of progress percentage.

--- progress.js
+++ progress.js
@@ -3,7 +3,13 @@ var tracksProgress = [50, 60, 35, 20, 15, 90];
 var tracksPercentage = document.querySelectorAll('.progress-bar');
 var tracksPercentageText = document.querySelectorAll('.progress-percentage');
 
+var tracksPercentageTotalText = document.querySelector('#progress-total');
+var tracksPercentageTotalValue = 0;
+
 for (var i = 0; i < tracksPercentage.length; i++) { 
     tracksPercentage[i].style.width = tracksProgress[i] + '%';
     tracksPercentageText[i].textContent = tracksProgress[i] + '%';
+    tracksPercentageTotalValue += tracksProgress[i];
 }
+
+tracksPercentageTotalText.textContent = 'Total: ' +  Math.round(tracksPercentageTotalValue/6) + '%';
