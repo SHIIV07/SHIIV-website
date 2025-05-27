b54badda3f1ee779fcff9d74738d08b615f0b279
SHIIV07 <mlynariktomo@gmail.com>
2025-05-27 14:15:48 +0200

    Site v1.5.0
    **Main Changes**
    
    - Project Jade Progress added to site
    - Removed Scarlet Bride promotional section
    
    **Nerdy Changes**
    
    - Cleared H/E/L/L preview audio from the site
    - Improved font-size variables
    - Made CSS use less modern ways to code so it is more
    accessible on more browsers
    - Minor graphic changes and other optimizations

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+// PERCENTAGE CALCULATIONS
 
+const phaseProgress = [75, 10, 0];
+
+const phasePercentage = document.querySelectorAll('.progress-bar');
+const phasePercentageText = document.querySelectorAll('.progress-percentage');
+
+const phasePercentageTotalText = document.querySelector('#progress-total');
+var phasePercentageTotalValue = 0;
+
+for (var i = 0; i < phasePercentage.length; i++) { 
+    phasePercentage[i].style.width = phaseProgress[i] + '%';
+    phasePercentageText[i].textContent = phaseProgress[i] + '%';
+    phasePercentageTotalValue += phaseProgress[i];
+}
+
+phasePercentageTotalText.textContent = 'Total: ' +  Math.round((phasePercentageTotalValue*100)/3)/100 + '%';
